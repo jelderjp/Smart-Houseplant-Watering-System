6d6a02d75b3b2ce580e92f26db33e97027a26dea
DDStudent <rfdg125@yahoo.com>
2026-03-24 13:57:44 -0600

    03/24/2026

---
+++ Watering_system/src/Watering_system.cpp
@@ -0,0 +1,24 @@
+/* 
+ * Project Smart Houseplant Watering System
+ * Author: Jay Elder
+ * Date: 03/19/2026
+ */
+
+
+#include "Particle.h"
+
+
+SYSTEM_MODE(AUTOMATIC);
+
+
+SYSTEM_THREAD(ENABLED);
+
+
+void setup() {
+  
+}
+
+
+void loop() {
+  
+}
